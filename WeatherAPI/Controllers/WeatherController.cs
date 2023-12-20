6fd00d35996ad4502eaddbc10c0fa7732dab7b82
Huda Ezeddin <hoda92ez@gmail.com>
2023-12-20 07:40:54 +0300

    Simple documentation of each end point.

--- WeatherAPI/Controllers/WeatherController.cs
+++ WeatherAPI/Controllers/WeatherController.cs
@@ -24,7 +24,19 @@ namespace WeatherAPI.Controllers
         }
 
 
-
+        /// <summary>
+        /// Retrieves weather data for a specific city.
+        /// </summary>
+        /// <remarks>
+        /// This endpoint allows you to get weather information for a specified city from the cach if it is found or using the Weather API.
+        /// </remarks>
+        /// <param name="city">The name of the city for which weather information is requested.</param>
+        /// <returns>
+        /// A <see cref="GenericResponse{CityWeatherQueryResponse}"/> containing the weather information for the specified city.
+        /// </returns>
+        /// <response code="200">Returns weather information for the specified city.</response>
+        /// <response code="400">If the request is malformed or if the city parameter is missing or there is no location found matching parameter .</response>
+        /// <response code="500">If there is an internal server error while processing the request.</response>
 
 
         [HttpGet("/weather/city")]
@@ -34,6 +46,22 @@ namespace WeatherAPI.Controllers
             return response;
         }
 
+
+        /// <summary>
+        /// Retrieves weather data for multiple cities in bulk.
+        /// </summary>
+        /// <remarks>
+        /// This endpoint allows you to get weather information for multiple cities simultaneously using the Weather API.
+        /// </remarks>
+        /// <param name="request">A <see cref="BulkRequest"/> object containing a list of cities for which weather information is requested.</param>
+        /// <returns>
+        /// A <see cref="GenericResponse{T}"/> containing the bulk weather information for the specified cities.
+        /// </returns>
+        /// <response code="200">Returns bulk weather information for the specified cities.</response>
+        /// <response code="400">If the request is malformed or if the request body is missing or invalid.</response>
+        /// <response code="500">If there is an internal server error while processing the request.</response>
+         
+
         [HttpPost("/weather/bulk")]
         public async Task<GenericResponse<BulkCommandResponse>> GetWeathereDataOfCities([FromBody] BulkRequest request)
         {
@@ -41,7 +69,18 @@ namespace WeatherAPI.Controllers
             return response;
         }
 
-
+        /// <summary>
+        /// Retrieves weather statistics for a specific time period.
+        /// </summary>
+        /// <remarks>
+        /// This endpoint allows you to get aggregated data like the average tempreture, highest temperature, and lowest temperature giving list of cities  using the Weather API.
+        /// </remarks>
+        /// <returns>
+        /// A <see cref="GenericResponse{T}"/> containing weather statistics for the specified time period.
+        /// </returns>
+        /// <response code="200">Returns weather statistics for the specified time period.</response>
+        
+        /// <response code="500">If there is an internal server error while processing the request.</response>
         [HttpGet("/weather/statistics")]
         public async Task<GenericResponse<WeatherStatisticResponse>> GetWeatherStatistics()
         {
